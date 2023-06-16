37af6b361fb047bf041a7fb6b2084eabf9ece847
bowie <thricyasilva@gmail.com>
2023-06-16 14:09:00 -0300

    adiciona a primeira versão do meu projeto

---
+++ mulheres.js
@@ -0,0 +1,34 @@
+const express = require("express")
+const router = express.Router() 
+
+const app = express()
+const porta = 3333
+
+const mulheres = [
+    {
+        nome: "Thricya Silva",
+        imagem: "https://media.licdn.com/dms/image/D4D03AQEQR2qWq3KyJg/profile-displayphoto-shrink_200_200/0/1669776478342?e=1692230400&v=beta&t=_U8pXbypQc75an7DvipkFhjfI5UA4mFjJeTGuzMwZyk",
+        minibio: "Desenvolvedora"
+    },
+    {
+        nome: "Iana Chan",
+        imagem: "",
+        minibio: "Fundadora da PrograMaria"
+    },
+    {
+        nome: "Nina da Hora",
+        imagem: "",
+        minibio: "Hacker antirracista"
+    }
+]
+
+function mostraMulheres(request, response) {
+    response.json(mulheres)
+}
+
+function mostraPorta() {
+    console.log("Servidor criado e rodando na porta ", porta)
+}
+
+app.use(router.get("/mulheres", mostraMulheres))
+app.listen(porta, mostraPorta)
